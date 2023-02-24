420ba76907a09cc2e27ef80944f7d20f0f441f6b
David Warren Katz <59783036+DavidWarrenKatz@users.noreply.github.com>
2023-02-24 15:51:14 -0500

    Update snmf.py

--- snmf.py
+++ snmf.py
@@ -6,7 +6,7 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io
-import hicstraw     #someitmes this is import hicstraw?
+import hicstraw
 import seaborn as sns
 from matplotlib import gridspec
 from matplotlib.colors import LinearSegmentedColormap
@@ -21,6 +21,7 @@ n = int(input("Input an integer n: "))
 matrix_object_chr4 = hic.getMatrixZoomData('4', '4', "observed", "NONE", "BP", 5000)
 
 #Get a numpy matrix for the loci between 10MB and 12MB. This is of type numpy.ndarray
+#This is a 401 by 401 matrix, represneting two million base paris at 5000 bp resolution
 V = matrix_object_chr4.getRecordsAsMatrix(10000000, 12000000, 10000000, 12000000)
 
 # import MATLAB module
@@ -30,59 +31,78 @@ eng = matlab.engine.start_matlab()
 # add MATLAB path
 eng.run_me_first(0, nargout=0)
 
+# convert numpy array to matlab.double
+V_m = matlab.double(V.tolist())
+
+# set options for symmetric solver
+options = dict()
+options['verbose'] = '1'
+options['max_epoch'] = '500'
+#options.calc_symmetry = true;    #this line might only work in the matlab code
+
+
+# convert dictioary to matlab.struct
+#options_m = eng.struct(options);
+
+#create array of ranks
+ranks = np.arange(0., n+1., 1)
+
+#create an empy array of costs to be filled in the following loop
+errors_mu = np.zeros(n+1)
+errors_hals = np.zeros(n+1)
+errors_symm_newton = np.zeros(n+1)
+errors_acc_hals = np.zeros(n+1)
+
 for i in range(n):
     # define rank to be factorized
-    rank = i
-
-    # set options for symmetric solver
-    options = dict()
-    options['verbose'] = '1'
-    options['max_epoch'] = '100'
-    options.calc_symmetry = true;    #this line might only work in the matlab code
-
-
-    # convert numpy array to matlab.double
-    V_m = matlab.double(V.tolist())
-
-    # convert dictioary to matlab.struct
-    options_m = eng.struct(options);
-
+    rank = i+1
+    
     # perform solvers in MATLAB
+    
     # Fro-MU
+    options['alg'] = 'mu'
+    options_m = eng.struct(options);
     [w_mu, infos_mu] = eng.fro_mu_nmf(V_m, rank, options_m, nargout=2)
-
-
+    cost_mu = list(infos_mu['cost'][0])
+    errors_mu[i] = cost_mu[0]
     
-    ## perform symmetric factroization
-    # Symm-ANLS
-    options.alpha = 0.6;
-    [w_symm_anls, infos_symm_anls] = eng.symm_anls(V, rank, options);
-    # Symm-Newton
-    [w_symm_newton, infos_symm_newton] = eng.symm_newton(V, rank, options);
-    # Symm-Hals
-    #options.lambda = 0.6;
-    #[w_symm_halsacc, infos_symm_halsacc] = eng.symm_halsacc(V, rank, options);
+    # HALS
+    options['alg'] = 'hals'
+    options_m = eng.struct(options);
+    [w_hals, infos_hals] = eng.als_nmf(V_m, rank, options_m, nargout=2)
+    cost_hals = list(infos_hals['cost'][0])
+    errors_hals[i] = cost_hals[0]
     
-    # convert matlab.struct to list for symmetric alg
-    iter_anls = list(infos_symm_anls['iter'][0])
-    cost_anls = list(infos_symm_anls['cost'][0])
-    iter_newton = list(infos_symm_newton['iter'][0])
-    cost_newton = list(infos_symm_newton['cost'][0])
-    #iter_hals = list(infos_symm_halsacc['iter'][0])
-    #cost_hals = list(infos_symm_halsacc['cost'][0])
+#    # Symm-Newton
+#    options['calc_symmetry'] = True;
+#    options['alg'] = 'symm_newton';
+#    options['alpha'] = 0.6;
+#    options_m = eng.struct(options);
+#    [w_symm_newton, infos_symm_newton] = eng.symm_newton(V_m, rank, options_m);
+#    cost_symm_newton = list(infos_symm_newton['cost'][0])
+#    errors_symm_neton[i] = cost_symm_newton[0]
     
-
-    # plotting of ANLS
-    plt.figure()
-    plt.xlabel('Epoch')
-    plt.ylabel('Cost')
-    plt.plot(iter_anls, cost_anls, label ="norm(W - Wt)")
-    plt.legend()
-    plt.title(f'Epoch vs. Cost for {i}')
-
-    plt.show()
-
-
+    # ACC-HALS
+    options['alg'] = 'acc_hals'
+    options_m = eng.struct(options);
+    [w_acc_hals, infos_acc_hals] = eng.als_nmf(V_m, rank, options_m, nargout=2)
+    cost_acc_hals = list(infos_acc_hals['cost'][0])
+    errors_acc_hals[i] = cost_acc_hals[0]
+    
+    
+    
+# plotting
+plt.figure()
+plt.xlabel('Rank')
+plt.ylabel('Cost')
+plt.plot(ranks, errors_mu, label ="Fro-MU")
+plt.plot(ranks, errors_hals, label ="HALS")
+#plt.plot(ranks, errors_symm_newton, label ="Symm-Newton")
+plt.plot(ranks, errors_hals, label ="ACC-HALS")
+plt.legend()
+plt.title('Rank vs. Cost')
+plt.show()
+    
 
 
 eng.quit()
